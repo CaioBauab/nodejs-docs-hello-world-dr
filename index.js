486a737d7558cb84285793c3fc71f4c7f8b3beb7
Caio Bauab <34510298+CaioBauab@users.noreply.github.com>
2025-02-05 17:43:13 -0300

    Update index.js

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ const router = express.Router();
 
 // Hello World for index page
 app.get('/', function (req, res) {
-    return res.send("PRD");
+    return res.send("DR");
 })
 
 app.get('/api', function (req, res) {
